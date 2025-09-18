c5592bc01a5962e53e25e14713e02669ca3248de
Rebeccakontio <157686034+Rebeccakontio@users.noreply.github.com>
2025-09-18 13:08:59 +0300

    feat: add sections

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,14 +1,23 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { Projects } from "@/sections/Projects";
+import { Technologies } from "@/sections/Technologies";
+import { Experience } from "@/sections/Experience";
+import { Contact } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
+import { ParticleBackground } from "@/components/ParticleBackground";
 
 export default function Home() {
   return (
     <div>
+      <ParticleBackground />
       <Header />
       <Hero />
       <Projects />
-      {/* Add other sections here */}
+      <Technologies />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
   );
 }
