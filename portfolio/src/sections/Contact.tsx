b627d1c9b8a2083e44e6218a4443884c1bfe635a
Rebeccakontio <157686034+Rebeccakontio@users.noreply.github.com>
2025-09-23 18:17:10 +0300

    feat: add contact section with contact button

--- portfolio/src/sections/Contact.tsx
+++ portfolio/src/sections/Contact.tsx
@@ -1,3 +1,34 @@
-export const ContactSection = () => {
-  return <div>Contact Section</div>;
+import { Button } from "@/components/Button";
+import  ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import FadeIn from "@/components/ScrollTrigger";
+
+export const Contact = () => {
+  return (
+    <div className="container lg:pt-12 lg:pb-20">
+      <FadeIn>
+        <div className="bg-gradient-to-r from-emerald-500 to-sky-400 text-gray-900 py-8 px-16
+        rounded-3xl text-center relative overflow-hidden z-0">
+          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center  md:justify-between">
+            <div>
+              <h2 className="font-bold text-3xl md:text-5xl md:text-left">Say Hi</h2>
+              <p className="mt-4 text-gray-900 md:text-left">
+                I am always open to discussing new projects and opportunities.
+              </p>
+            </div>
+            <div className="mt-6">
+              <a
+                href="mailto:kontiorebecca@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button color="transparent" icon={<ArrowUpRightIcon />}>
+                  Contact
+                </Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </FadeIn>
+    </div>
+  );
 };
