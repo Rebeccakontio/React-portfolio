00fe5ff1e485b9602405aeca97115e085a125ee6
Rebeccakontio <157686034+Rebeccakontio@users.noreply.github.com>
2025-09-23 14:46:23 +0300

    feat: add Header and Footer components with navigation links and social media icons

--- portfolio/src/sections/Footer.tsx
+++ portfolio/src/sections/Footer.tsx
@@ -1,3 +1,31 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="relative bg-gradient-to-t from-cyan-600/30 via-[#1e293b]/60 via-60% to-transparent">
+      <div className="container pt-12 ">
+        <div className="py-10 flex flex-col items-center">
+          <span>© 2025 Rebecca Kontio. All rights reserved.</span>
+          <nav className="mt-4 inline-flex items-center gap-2">
+            <a
+              href="https://github.com/Rebeccakontio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHubIcon fontSize="large" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rebecca-kontio-614230291/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedInIcon fontSize="large" />
+            </a>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
--- portfolio/src/sections/Header.tsx
+++ portfolio/src/sections/Header.tsx
@@ -1,17 +1,20 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center fixed top-3 w-full">
-      <nav className="flex gap-1 p-0.5 border border-white/10 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav">
-          Home
-        </a>
-        <a href="#" className="nav">
+    <div className="flex justify-center items-center fixed top-4 w-full z-50">
+      <nav className="flex gap-1 p-0.5 border border-white/10 rounded-full bg-white/20 backdrop-blur">
+        <a href="#about" className="nav">
           About
         </a>
-        <a href="#" className="nav">
+        <a href="#projects" className="nav">
           Projects
         </a>
-        <a href="#" className="nav bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+        <a href="#experience" className="nav">
+          Experience
+        </a>
+        <a
+          href="#contact"
+          className="nav bg-white text-gray-900 hover:bg-cyan-400 hover:text-gray-900"
+        >
           Contact
         </a>
       </nav>
